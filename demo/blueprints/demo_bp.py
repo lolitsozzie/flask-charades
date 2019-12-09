0d8389a7e7776aaf064f10b74c62250c849ea79d
lolitsozzie <ozzie.14.martin@gmail.com>
2019-12-09 14:55:04 -0500

    add front end play page

--- demo/blueprints/demo_bp.py
+++ demo/blueprints/demo_bp.py
@@ -14,16 +14,20 @@ def index():
 
 @demo_bp.route('/addCategory', methods=['GET', 'POST'])
 def addCategory():
+    category = ''
+    correctlyAdded = False
     if request.method == 'POST':
         form_dict = request.form.to_dict()
         categories = [c.name for c in Category.get_all()]
+        category = form_dict['category']
         if form_dict['category'] not in categories:
             category = Category(
                 name=form_dict['category']
             )
+            correctlyAdded = True
             db.session.add(category)
             db.session.commit()
-    return render_template('addCategory.html')
+    return render_template('addCategory.html', category=category, correctlyAdded=correctlyAdded)
 
 
 @demo_bp.route('/addWord', methods=['GET', 'POST'])
@@ -31,6 +35,8 @@ def addWord():
     categories = Category.get_all()
     sameWord = False
     correctlyAdded = False
+    previousWord = ''
+    categoryId = 1000
     if request.method == 'POST':
         form_dict = request.form.to_dict()
         categoryId = int(form_dict['category'])
@@ -39,13 +45,15 @@ def addWord():
             text=form_dict['word'],
             categoryId=categoryId
         )
+        previousWord = word.text
         if word.text in words:
             sameWord = True
         if word.text not in words:
             correctlyAdded = True
             db.session.add(word)
             db.session.commit()
-    return render_template('addWord.html', categories=categories, sameWord=sameWord, correctlyAdded=correctlyAdded, word=(word.text if word else ''))
+    return render_template('addWord.html', categories=categories, sameWord=sameWord, correctlyAdded=correctlyAdded,
+                           word=previousWord, categoryId=categoryId)
 
 
 @demo_bp.route('/viewWords', methods=['GET', 'POST'])
@@ -59,3 +67,18 @@ def viewWords():
         words = [x for x in words if x.categoryId == category]
         pass
     return render_template('viewWords.html', categories=categories, words=words, category=category)
+
+@demo_bp.route('/play', methods=['GET', 'POST'])
+def play():
+    categories = Category.get_all()
+    playWords = []
+    words = Word.get_all()
+    category = 1000
+    if request.method == 'POST':
+        form_dict = request.form.to_dict()
+        play_categories = [int(x) for x in form_dict.values() if x.isdigit()]
+        for x in play_categories:
+            for y in words:
+                if y.id == x:
+                    playWords.append(y)
+    return render_template('play.html', category=category, categories=categories, playWords=playWords)
